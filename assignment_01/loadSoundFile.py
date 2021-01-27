e00002d1883cb14c22660f2c01b677d899d566cb
Sophia mehdizadeh <smehdizadeh7@gatech.edu>
2021-01-27 00:09:25 -0500

    added load sound file function

--- assignment_01/loadSoundFile.py
+++ assignment_01/loadSoundFile.py
@@ -5,3 +5,19 @@ Created on Tue Jan 26 22:40:56 2021
 @author: sophi
 """
 
+# Imports
+import scipy.io as sio
+import numpy as np
+
+'''
+Function to load audio files
+Input: filepath (string)
+Output: x 1D numpy array (float)
+'''
+def loadSoundFile(filename):
+    # read audio file and store as ndarray data
+    sampRate, data = sio.wavfile.read(filename)
+    
+    # reduce to 1 channel and convert to float
+    x = np.array(data[:,0], dtype=float)
+    return x
