85f7a148d42b136e2d957a4d0ec82e5e811038d6
Sophia mehdizadeh <smehdizadeh7@gatech.edu>
2021-01-26 22:42:38 -0500

    initialize files for assignment 1

---
+++ assignment_01/crossCorr.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 22:36:42 2021
+
+@author: sophi
+"""
---
+++ assignment_01/loadSoundFile.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 22:40:56 2021
+
+@author: sophi
+"""
+
